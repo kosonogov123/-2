95fea54aac464245337f1b534099dd2d8b00bc9a
kosonogov123 <122813241+kosonogov123@users.noreply.github.com>
2023-01-31 15:24:28 +0300

    исправлена 13 задача

--- задача 13/Program.cs
+++ задача 13/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите число");
 int n =Convert.ToInt32(Console.ReadLine());
-if (n/100 > 0)
-Console.WriteLine((n/10)%10);
-else
+if (n/100 == 0)
 Console.WriteLine("Третьей цифры нет");
+
+else 
+{{while (n>=1000)
+n=n/10;}
+Console.WriteLine(n%10);}
